0997ac2e2039f3def849574b9b21bdba02bc4ff3
Tom Faulhaber <git_net@infolace.com>
2009-01-25 18:41:06 -0800

    Moved utility functions into a separate namespace

--- com/infolace/format.clj
+++ com/infolace/format.clj
@@ -7,6 +7,7 @@
 ;   You must not remove this notice, or any other, from this software.
 
 (ns com.infolace.format
+  (:use com.infolace.format.utilities)
   (:import [com.infolace.format FormatException InternalFormatException
 	    ColumnWriter]))
 
@@ -17,96 +18,11 @@
 ;;; End forward references
 
 (defn cl-format 
-  "An implementation of a (mostly) Common Lisp compatible format function"
+  "An implementation of a Common Lisp compatible format function"
   [stream format-in & args]
   (let [compiled-format (if (string? format-in) (compile-format format-in) format-in)
 	navigator (init-navigator args) ]
     (execute-format stream compiled-format navigator)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Helper functions for digesting formats in the various
-;;; phases of their lives.
-;;; These functions are actually pretty general.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn map-passing-context [func initial-context lis]
-  (loop [context initial-context
-	 lis lis
-	 acc []]
-    (if (empty? lis)
-      [acc context]
-    (let [this (first lis)
-	  remainder (rest lis)
-	  [result new-context] (apply func [this context])]
-      (recur new-context remainder (conj acc result))))))
-
-(defn consume [func initial-context]
-  (loop [context initial-context
-	 acc []]
-    (let [[result new-context] (apply func [context])]
-      (if (not result)
-	[acc new-context]
-      (recur new-context (conj acc result))))))
-
-(defn consume-while [func initial-context]
-  (loop [context initial-context
-	 acc []]
-    (let [[result continue new-context] (apply func [context])]
-      (if (not continue)
-	[acc context]
-      (recur new-context (conj acc result))))))
-
-(defn unzip-map [m]
-  "Take a  map that has pairs in the value slots and produce a pair of maps, 
-   the first having all the first elements of the pairs and the second all 
-   the second elements of the pairs"
-  [(into {} (for [[k [v1 v2]] m] [k v1]))
-   (into {} (for [[k [v1 v2]] m] [k v2]))])
-
-(defn tuple-map [m v1]
-  "For all the values, v, in the map, replace them with [v v1]"
-  (into {} (for [[k v] m] [k [v v1]])))
-
-(defn rtrim [s c]
-  "Trim all instances of c from the end of sequence s"
-  (let [len (count s)]
-    (if (and (pos? len) (= (nth s (dec (count s))) c))
-      (loop [n (dec len)]
-	(cond 
-	 (neg? n) ""
-	 (not (= (nth s n) c)) (subs s 0 (inc n))
-	 true (recur (dec n))))
-      s)))
-
-(defn ltrim [s c]
-  "Trim all instances of c from the beginning of sequence s"
-  (let [len (count s)]
-    (if (and (pos? len) (= (nth s 0) c))
-      (loop [n 0]
-	(if (or (= n len) (not (= (nth s n) c)))
-	  (subs s n)
-	  (recur (inc n))))
-      s)))
-
-(defn prefix-count [aseq val]
-  "Return the number of times that val occurs at the start of sequence aseq, 
-if val is a seq itself, count the number of times any element of val occurs at the
-beginning of aseq"
-  (let [test (if (coll? val) (set val) #{val})]
-    (loop [pos 0]
-     (if (or (= pos (count aseq)) (not (test (nth aseq pos))))
-       pos
-       (recur (inc pos))))))
-
-(defn- prerr [& args]
-  "Println to *err*"
-  (binding [*out* *err*]
-    (apply println args)))
-       
-(defmacro prlabel [prefix arg & more-args]
-  "Print args to *err* in name = value format"
-  (cons 'prerr (cons (list 'quote prefix) (mapcat #(list (list 'quote %) "=" %) 
-						  (cons arg more-args)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Argument navigators manage the argument list
@@ -1431,9 +1347,6 @@ first character of the string even if it's a letter."
 	      (nil? (:rest navigator)))
 	  [exit navigator] navigator))))) 
   )
-
-(defn my-status [] 
-  (println "There are" (count directive-table) "out of 33 directives implemented"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code to manage the parameters and flags accociated with each
